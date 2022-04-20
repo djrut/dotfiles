7a520aaa809d1fa3e4ec3a2bc8f5c9e9f9f75383
Duncan Rutland <djrut@google.com>
2022-04-20 15:02:08 -0500

    Reorganized layout

--- macbook_setup.sh
+++ macbook_setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-brew install $(cat brew.txt)
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 echo "Initializing global git config..."
 git config --global user.name "Duncan Rutland"
 git config --global user.email "djrut@google.com"
@@ -15,15 +11,17 @@ cd && git init && \
 	git remote add origin https://github.com/djrut/dotfiles.git && \
 	git pull origin master
 
+# Brew packages
+brew install $(cat brew.txt)
+$(brew --prefix)/opt/fzf/install
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # tmux tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # gcloud SDK
 curl https://sdk.cloud.google.com | bash
-
-# fzf
-
-# Vim
 
 # Powerline fonts - follow instructions https://github.com/wernight/powerline-web-fonts
 # term_.prefs_.set('font-family', '"DejaVu Sans Mono", monospace');

876790ede55a087246a94812e72bfda739b61b82
Duncan Rutland <djrut@google.com>
2022-05-26 13:16:52 -0400

    Added git pull for dotfiles

--- linux_quick_setup.sh
+++ linux_quick_setup.sh
@@ -2,13 +2,19 @@
 
 sudo apt-get update
 sudo apt-get -qy install git tmux direnv ctags cmake \
-  tree zsh curl yamllint host \
+  tree zsh curl yamllint host dnsutils \
   silversearcher-ag ripgrep
 
 echo "Initializing global git config..."
 git config --global user.name "Duncan Rutland"
 git config --global user.email "djrut@google.com"
 git config --global status.submoduleSummary true
+
+# Pull down dotfiles 
+echo "Pulling down dotfiles..."
+cd && git init && \
+	git remote add origin https://github.com/djrut/dotfiles.git && \
+	git pull origin master
 
 #Fuzzy wuzzy
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf

49556c73ec81d577a9a4fe670d2e885e47fa0c2c
Duncan Rutland <djrut@google.com>
2023-01-06 12:44:25 -0600

    Added comments/markers, added pyenv/python installation

--- linux_quick_setup.sh
+++ linux_quick_setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-
+# Globals {{{
+PYTHON_VERSION="3.11.1"
+# }}}
+# Install packages {{{
 sudo apt-get update && sudo apt-get install -qy gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
@@ -11,33 +14,42 @@ sudo apt-get update && sudo apt-get -qy install terraform \
   build-essential zlib1g-dev libffi-dev \
   libssl-dev libbz2-dev libreadline-dev \
   libsqlite3-dev liblzma-dev
-
+# }}}
+# Global git config {{{
 echo "Initializing global git config..."
 git config --global user.name "Duncan Rutland"
 git config --global user.email "djrut@google.com"
 git config --global status.submoduleSummary true
-
-# Save original dotfiles
+# }}}
+# Save original configuration files {{{
 mv .bashrc .bashrc.bak
 mv .profile .profile.bak
 mv .zshrc .zshrc.bak
-
-# Pull down dotfiles 
+# }}}
+# Pull down dotfiles {{{
 echo "Pulling down dotfiles..."
 cd && git init && \
 	git remote add origin https://github.com/djrut/dotfiles.git && \
 	git pull origin master
-
-#Fuzzy wuzzy
+# }}}
+# Install FZF {{{
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
-
+# }}}
+# Source new .bashrc {{{
 source ~/.bashrc
-
-# Install pyenv and Python
+# }}}
+# Install pyenv and Python {{{
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.11.1
-pyenv global 3.11.1
-
-# Install Vim plugins
+PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install ${PYTHON_VERSION}
+pyenv global ${PYTHON_VERSION}
+# }}}
+# Install TMUX plugin manager {{{
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# }}}
+# Install Vim plugins {{{
 vim +'PlugInstall --sync' +qall
+# }}}
+# Metadata {{{
+# vim:foldmethod=marker:foldlevel=0
+# }}}
